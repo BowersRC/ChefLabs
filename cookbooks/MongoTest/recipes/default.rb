93dcf018b18c27e566d80957bd25e8c656293f89
root <root@ip-172-31-24-208.ec2.internal>
2017-03-11 10:30:52 -0500

    Update with Mongo Repo file and recipe working

--- cookbooks/MongoTest/recipes/default.rb
+++ cookbooks/MongoTest/recipes/default.rb
@@ -1,7 +1,16 @@
 #
 # Cookbook:: MongoTest
-# Recipe:: default
+# Recipe:: MongoDB install and start
 #
 # Copyright:: 2017, The Rob Bowers, All Rights Reserved.
+# 
+# Install MongoDB.
+package "mongodb-org" do
+  action :install
+end
 
+# Start MongoDB.
+service "mongod" do
+  action [:start, :enable]
+end
 
